1d16ee625cc18dc848ca87292bccb36c5726df3f
dasom <dev.dasom1012@gmail.com>
2019-05-30 21:41:40 +0900

    수정: 리스트 컨테이너 삭제

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,67 @@
-import React from 'react';
-import List from '../containers/ListContainer';
+import React, { Component } from 'react';
+import axios from 'axios';
+import List from './List';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 1,
+      isLoading: false,
+      listData: []
+    }
+  }
 
+  componentDidMount() {
+    this.fetchData();
+    window.scrollTo(0, 0);
+    window.addEventListener("scroll", this.handleScroll);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <List />
-    </div>
-  );
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  fetchData = () => {
+    const { page, listData } = this.state
+    axios.get(`http://s3.ap-northeast-2.amazonaws.com/bucketplace-coding-test/cards/page_${page}.json`)
+      .then(res => {
+        this.setState({
+          page: page + 1,
+          listData: [...listData , ...res.data]
+        });
+        this.changeLoadingStatus(false);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  handleScroll = () => {
+    const { isLoading } = this.state;
+    let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+    let scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+    let clientHeight = document.documentElement.clientHeight;
+    if (scrollTop + clientHeight >= scrollHeight - 300) {
+      if(!isLoading) {
+        this.fetchData();
+        this.changeLoadingStatus(true);
+      } else {
+        return false;
+      }
+    };
+  }
+  changeLoadingStatus = (bool) => {
+    this.setState({
+      isLoading: bool
+    });
+  }
+  render() { 
+    const { listData } = this.state
+    return (
+      <List listData={listData}/>
+    );
+  }
 }
-
+ 
 export default App;
